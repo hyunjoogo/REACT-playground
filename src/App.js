e69dec9f0e474431e0fb3eb0bb9f88825f3d7583
HyunJoo Kim <hyunjoogo@gmail.com>
2022-09-30 18:14:50 +0900

    무한 스크롤 이벤트 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ShakeInput from "./shakeInput/ShakeInput";
 import Otp from "./OTP/Otp";
+import ScrollEvent from "./scroll-event/ScrollEvent";
 
 
 function App() {
   return (
     // <ShakeInput/>
-    <Otp/>
+    // <Otp/>
+    <ScrollEvent/>
   );
 }
 
